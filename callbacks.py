8b46cb09bfdf37ae35aff1f5429b5f418ff21513
xly135846 <30425553+xly135846@users.noreply.github.com>
2019-02-27 09:18:55 +0800

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -6,6 +6,6 @@ from parameters import *
 # checkpoint = ModelCheckpoint(filepath=checkpoint_file, save_weights_only=True, save_best_only=True, verbose=2)
 tensorborad = TensorBoard(log_dir=log_path)
 early_stop = EarlyStopping(patience=10)
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', patience=5, verbose=2, factor=0.99, min_lr=0.00001)
+reduce_lr = ReduceLROnPlateau(monitor='val_', patience=5, verbose=2, factor=0.99, min_lr=0.00001)
 
 # callback_list = [checkpoint, tensorborad, early_stop]
